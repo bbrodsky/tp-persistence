5cfb680ece083e4f054c868e96214a4f2ef0e5f1
bbrodsky <bbrodsky@gmail.com>
2016-10-05 18:04:27 -0400

    Nearing end of Day

--- routes/api/days.js
+++ routes/api/days.js
@@ -6,16 +6,44 @@ var Activity = require('../../models/activity');
 var Day = require('../../models/day');
 
 dayRouter.get("/", function(req, res, next) {
-  res.send("HELLOOOOO")
-  // Day.findAll()
-  //   .then(function (days){
-  //     res.json(days)
-  //   })
-  //   .catch(next)
+  Day.findAll()
+    .then(function (days){
+      res.json(days)
+    })
+    .catch(next)
+})
+
+dayRouter.post("/", function(req, res, next) {
+  Day.findAll()
+    .then(function(days) {
+      Day.create({
+        number: days.length+1
+      });
+    })
+    .catch(next);
+})
+
+dayRouter.get("/:id", function(req, res, next) {
+  Day.findById(req.params.id)
+    .then(function (day){
+      res.json(day);
+    })
+    .catch(next)
+})
+
+dayRouter.delete("/:id", function(req, res, next) {
+  Day.findById(req.params.id)
+    .then(function (day){
+      day.destroy();
+    })
+    .then(function() {
+      res.sendStatus(204);
+    })
+    .catch(next)
 })
 
 
-dayRouter.post('/:id/restaurants', function (req, res, next) {
+dayRouter.post('/:id/:attraction', function (req, res, next) {
   Day.findById(req.params.id)
     .then(function (day) {
       console.log(day)
